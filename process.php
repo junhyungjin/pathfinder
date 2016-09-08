42165ca2d2b51ccc98ac54c9e6d69cced1b4d46b
Hyung Jin Jun <junhyungjin@gmail.com>
2016-09-09 02:26:33 +0900

    add security

--- process.php
+++ process.php
@@ -1,11 +1,16 @@
 <?php
 $conn = mysqli_connect("localhost", "root", 111111);
 mysqli_select_db($conn, "pathfinder");
-$sql = "SELECT * FROM user WHERE name='".$_POST['author']."'";
+
+$title = mysqli_real_escape_string($conn, $_POST['title']);
+$author = mysqli_real_escape_string($conn, $_POST['author']);
+$description = mysqli_real_escape_string($conn, $_POST['description']);
+
+$sql = "SELECT * FROM user WHERE name='".$author."'";
 $result  = mysqli_query($conn, $sql);
 
 if($result->num_rows == 0){
-  $sql = "INSERT INTO user (name, password) VALUES('".$_POST['author']."', '111111')";
+  $sql = "INSERT INTO user (name, password) VALUES('".$author."', '111111')";
   mysqli_query($conn, $sql);
   $user_id = mysqli_insert_id($conn);
 } else {
@@ -13,7 +18,7 @@ if($result->num_rows == 0){
   $user_id = $row['id'];
 }
 
-$sql = "INSERT INTO topic (title,description,author,created) VALUES('".$_POST['title']."', '".$_POST['description']."', '".$user_id."', now())";
+$sql = "INSERT INTO topic (title,description,author,created) VALUES('".$title."', '".$description."', '".$user_id."', now())";
 $result = mysqli_query($conn, $sql);
 header('Location: http://localhost:8080/index.php?id=6');
 ?>
